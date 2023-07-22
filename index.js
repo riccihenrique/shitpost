7294f23c87184a06f7d675e1bb465db11698304c
Henrique Ricci <ricci.henrique@outlook.com>
2023-07-22 13:35:12 -0300

    test

--- index.js
+++ index.js
@@ -36,12 +36,13 @@ wa.create({
 
 function start(client) {
 
-  nodeCron.schedule("34 16 * * *", async () => {
+  nodeCron.schedule("37 16 * * *", async () => {
     const groups = await client.getAllGroups();
     await convertVideo(`https://www.youtube.com/watch?v=${fixos.oleoDeMacaco}`, fixos.oleoDeMacaco)
 
     groups.forEach((group) => {
       //client.sendFile(group.id, `${fixos.oleoDeMacaco}.mp4`, `${fixos.oleoDeMacaco}.mp4`, '');
+      console.log(group)
       client.sendFile('5518988247731@c.us', `${fixos.oleoDeMacaco}.mp4`, `${fixos.oleoDeMacaco}.mp4`, '');
 
     })
